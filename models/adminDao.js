b053183cdfbf14f151658fc1f11842e4f09a42b4
hejine <3155159272@qq.com>
2017-09-13 13:09:22 +0800

    hejin

---
+++ models/adminDao.js
@@ -0,0 +1,75 @@
+var pool=require("./dbConnection");
+var adminDao={
+		//查询数据 列表
+		query:function(callback){
+			var sql="select*from administrator ";
+			
+			pool.getConnection(function(err,connection){
+				if(err){
+					console.log("查询失败");
+				}else{
+					connection.query(sql,function(err,result){
+						callback(err,result);
+					});
+				}
+			});
+		},
+		//增加数据
+		add:function(data,callback){
+			var sql="insert into administrator(account,password,authority)value(?,?,2)";
+			pool.getConnection(function(err,connection){
+				if(err){
+					console.log("连接失败");
+				}else{
+					connection.query(sql,data,function(err,result){
+						callback(err,result);
+					});
+				}
+			});
+		},
+		//更改数据
+		update:function(data,callback){
+			var sql="update administrator set account=?,password=?,authority=? where id=?";
+		//	var upSql=[data.account,data.password,data.authority,data.id];
+			pool.getConnection(function(err,connection){
+				if(err){
+					console.log("更新失败");
+				}else{
+					connection.query(sql,data,function(err,result){
+						callback(err,result);
+					});
+				}
+			});
+		},
+		//删除数据
+		del:function(data,callback){
+			var sql="delete from administrator where id=?";
+		//	var delSql=[data.id];
+			pool.getConnection(function(err,connection){
+				if(err){
+					console.log("删除失败");
+				}else{
+					connection.query(sql,data,function(err,result){
+						callback(err,result);
+					});
+				}
+			});
+		},
+		//账号是否重复
+		isexist:function(data,callback){
+			var sql="select * from administrator where account=? ";
+		//	var reSql=[data.account];
+			pool.getConnection(function(err,connection){
+				if(err){
+					console.log("登录失败")
+				}else{
+					connection.query(sql,data,function(err,result){
+						callback(err,result);
+					});
+				}
+			});
+			
+		}
+}
+
+module.exports=adminDao;
---
+++ routes/admin.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+var adminDao=require("../models/adminDao");
+
+router.post("./add",function(req,resp,next){    // 添加管理员
+	var data=[req.body.account,req.body.password,req.authority];
+	adminDao.add(data,function(err,result){
+		console.log(err,result);
+		var o={};
+		o.result=err==null;
+		o.massage=o.result?"添加管理员成功":"添加失败";	
+		console.log(o);
+		resp.json(o);
+	});
+	next();
+});
+
+router.post("./update",function(req,resp,next){    //修改管理员
+	var data=[req.body.account,req.body.password,req.body.authority,req.body.id];
+	adminDao.update(data,function(err,result){
+		var o={};
+		o.result=err=null;
+		o.massage=o.result?"更改管理员成功":"更改失败";
+		resp.json(o);
+	});
+	next();
+});
+
+router.post("./del",function(req,resq,next){   //删除管理员
+	var data=[req.body.id];
+	adminDao.del(data,function(err,result){
+		var o={};
+		o.result=err==null;
+		o.massage=o.result?"删除成功":"删除失败";
+		o.data=result;
+		resq.json(o);
+	});
+	next();
+});
+
+router.get("./list",function(req,resp,next){  // 查询管理员表
+	adminDao.query(function(err,result){
+		var o={};
+		o.result=err==null;
+		o.massage=o.result?"查询管理员成功":"查询失败";	
+		o.data=result;
+		resp.json(o);
+	});
+	next();
+});
+
+router.get("./isexist",function(req,resp,next){
+	var data=[req.body.account];
+	adminDao.isexist(data,function(err,result){
+		var o={};
+		o.result=true==null;
+		o.massage=o.result=""?"成功，可以使用":"账号已重复";
+		o.data=result;
+	});
+});
+
+/* GET users listing.
+router.get('/add', function(req, res, next) {
+  res.send('respond with a resource');
+});
+ */
+module.exports = router;
+
+/**if exists (select * from syscolumns where name='colname1' and id=object_id('数据库名.Owner.表名'))
+　　　　print '存在'
+　　else
+　　　　print '不存在'
+ * **/
