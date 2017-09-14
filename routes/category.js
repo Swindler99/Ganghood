fdba272863d4edbd88ea2dd524f6eae2a4fb21fa
LanceLDW <420149109@qq.com>
2017-09-14 10:26:53 +0800

    123

--- routes/category.js
+++ routes/category.js
@@ -2,16 +2,50 @@ var express = require('express');
 var router = express.Router();
 var categoryDao=require("../models/categoryDao");
 /* GET category listing. */
-router.post('/add', function(req, res, next) {
+router.post('/add', function(req, res) {
   var data=[req.body.name,req.body.categoryId];
   categoryDao.add(data,function(err,result){
   	var o={};
   	o.result=err==null;
   	o.msg=o.result?"增加成功":"增加失败";
-  	o.data='';
+  	o.data={data:result};
   	res.json(o);
   })
-  next();
+  
+});
+
+router.get('/list/:categoryId',function(req,res){
+	var data=[req.params.categoryId];
+	categoryDao.query(data,function(err,result){
+		console.log(err);
+		var o={};
+		o.result=err==null;
+	  	o.msg=o.result?"查询成功":"查询失败";
+	  	o.data={data:result};
+	  	res.json(o);
+	})
+});
+
+router.post('/update',function(req,res){
+	var data=[req.body.name,req.body.categoryId,req.body.id];
+	categoryDao.update(data,function(err,result){
+		var o={};
+		o.result=err==null;
+	  	o.msg=o.result?"修改成功":"修改失败";
+	  	o.data={data:result};
+	  	res.json(o);
+	})
+});
+
+router.post('/del/:id',function(req,res){
+	var data=[req.params.id];
+	categoryDao.del(data,function(err,result){
+		var o={};
+		o.result=err==null;
+	  	o.msg=o.result?"删除成功":"删除失败";
+	  	o.data={data:result};
+	  	res.json(o);
+	})
 });
 
 module.exports = router;
