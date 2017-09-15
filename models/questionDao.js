2e25fe650dbd7f208797c63d752f551fee8937d3
LanceLDW <420149109@qq.com>
2017-09-15 09:59:26 +0800

    问题数据访问模型修改

--- models/questionDao.js
+++ models/questionDao.js
@@ -1,63 +1,56 @@
 var pool=require("./dbConnection");
 var questiondao ={
-		//增加问题
-		add: function(data,callback) {
-			var sql="insert into question( title，content，top，reportTime，collectedTime，urgent，score，anonymous，time，addContent，userId，attention，category) value(?,?,2,?,?,2,?,2,?,?,?,?,?)";
-			var sqlParams=[data.title,data.content,data.urgent,data.score,data.anonymous,data.category,data.userid];
-			pool.getConnection(function(err,connection){
-			if(err){
-				console.log("连接失败!")
-			}else{
-				connection.query(sql,sqlParams,function(err,result){
-					callback(err,result);
-				})
-			}
-		})	
-		},
-		//删除问题
-  del：function(data,callback){
-			var sql="delete from question (id) where question.id=? ";
-			var sqlParams=[data.id];
+	//增加问题
+	add: function(data,callback) {
+		var sql="insert into question(title,content,top,reportTime,collectedTime,urgent,score,anonymous,time,userId,attention,category) value(?,?,2,0,0,?,?,?,now(),?,2,?)";
 		pool.getConnection(function(err,connection){
 			if(err){
 				console.log("连接失败!")
 			}else{
-				connection.query(sql,sqlParams,function(err,result){
+				connection.query(sql,data,function(err,result){
+					callback(err,result);
+				})
+			}	
+		})	
+	},
+	//删除问题
+	del：function(data,callback){
+		var sql="delete from question where id=? ";
+		pool.getConnection(function(err,connection){
+			if(err){
+				console.log("连接失败!")
+			}else{
+				connection.query(sql,data,function(err,result){
 					callback(err,result);
 				})
 			}
 		})	
 		},
-		//追加问题
-		add2: function(data,callback) {
-			var sql="update question set addContent=? where question.id=?"
-            var sqlParams=[data.id];
-			var sqlParams=[data.addContent];
-			pool.getConnection(function(err,connection){
+	//追加问题
+	add2: function(data,callback) {
+		var sql="update question set addContent=? where id=?"
+		pool.getConnection(function(err,connection){
 			if(err){
 				console.log("连接失败!")
 			}else{
-				connection.query(sql,sqlParams,function(err,result){
+				connection.query(sql,data,function(err,result){
 					callback(err,result);
 				})
 			}
 		})	
 		},
 	//查看问题	
-
-		view：function(data,callback){
-			var sql="select*from question where id=? ";
-			var sqlParams=[data.id];
+	view：function(data,callback){
+		var sql="SELECT q.*,c.name AS categoryName FROM question AS q JOIN category AS c ON q.category=c.id WHERE q.id=?";
 		pool.getConnection(function(err,connection){
 			if(err){
 				console.log("连接失败!")
 			}else{
-				connection.query(sql,sqlParams,function(err,result){
+				connection.query(sql,data,function(err,result){
 					callback(err,result);
 				})
 			}
 		})	
 		},
-
 	}
 module.exports=questiondao;
