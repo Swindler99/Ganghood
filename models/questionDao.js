3e477041004b6f9613daffa31917536c58ce1b22
LanceLDW <420149109@qq.com>
2017-09-15 11:56:33 +0800

    增加置顶问题，举报数据访问对象方法

--- models/questionDao.js
+++ models/questionDao.js
@@ -1,7 +1,8 @@
 var pool=require("./dbConnection");
+var async=require("async");
 var questiondao ={
 	//增加问题
-	add: function(data,callback) {
+	add:function(data,callback) {
 		var sql="insert into question(title,content,top,reportTime,collectedTime,urgent,score,anonymous,time,userId,attention,category) value(?,?,2,0,0,?,?,?,now(),?,2,?)";
 		pool.getConnection(function(err,connection){
 			if(err){
@@ -14,7 +15,7 @@ var questiondao ={
 		})	
 	},
 	//删除问题
-	del：function(data,callback){
+	del:function(data,callback){
 		var sql="delete from question where id=? ";
 		pool.getConnection(function(err,connection){
 			if(err){
@@ -27,7 +28,7 @@ var questiondao ={
 		})	
 		},
 	//补充问题
-	supplement: function(data,callback) {
+	supplement:function(data,callback) {
 		var sql="update question set addContent=? where id=?"
 		pool.getConnection(function(err,connection){
 			if(err){
@@ -40,7 +41,7 @@ var questiondao ={
 		})	
 		},
 	//查看问题	
-	view：function(data,callback){
+	view:function(data,callback){
 		var sql="SELECT q.*,c.name AS categoryName FROM question AS q JOIN category AS c ON q.category=c.id WHERE q.id=?";
 		pool.getConnection(function(err,connection){
 			if(err){
@@ -53,7 +54,7 @@ var questiondao ={
 		})	
 	},
 	//用户发布问题列表
-	userQuery：function(data,callback){
+	userQuery:function(data,callback){
 		var sql="select * from question where userId=?";
 		pool.getConnection(function(err,connection){
 			if(err){
@@ -90,6 +91,53 @@ var questiondao ={
 				})
 			}
 		})
+	},
+	//置顶问题
+	top:function(data,callback){
+		async.waterfall([
+		    function(done){
+		    	var sql="select * from question where id=?";
+		    	pool.getConnection(function(err,connection){
+					if(err){
+						console.log("连接失败!")
+					}else{
+						connection.query(sql,data,function(err,result){
+							done(err,result[0].top);
+						})
+					}
+				});
+		    },function(onearg,done){
+		    	if(onearg==1){
+		    		onearg=2;
+		    	}else if(onearg==2){
+		    		onearg=1;
+		    	}
+		    	var sql="update question set top=? where id=?";
+		    	pool.getConnection(function(err,connection){
+					if(err){
+						console.log("连接失败!")
+					}else{
+						connection.query(sql,[onearg,data[0]],function(err,result){
+							done(err,result);
+						})
+					}
+				});
+		    }       
+		],function(err,result){
+			callback(err,result);
+		});
+	},
+	report:function(data,callback){
+		var sql="update question set reportTime=reportTime+1 where id=?";
+		pool.getConnection(function(err,connection){
+			if(err){
+				console.log("连接失败!")
+			}else{
+				connection.query(sql,data,function(err,result){
+					callback(err,result);
+				})
+			}
+		});
 	}
 }
 module.exports=questiondao;
