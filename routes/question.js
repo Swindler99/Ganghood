8c2b98e0141ecc96c9186d70ba1361f6f7f0b291
LanceLDW <420149109@qq.com>
2017-09-15 10:00:05 +0800

    问题路由修改

--- routes/question.js
+++ routes/question.js
@@ -1,57 +1,45 @@
 var express=require('express');
-
 var router=express.Router();
 var questionDao=require("../models/questionDao");
 
 //增加问题
-router.post('/add', function(req,res,next) {
-    var data=[req.body.title,req.body.content,req.body.top,req.body.reportTime，req.body.collectedTime，req.body.urgent，req.body.score，req.body.anonymous，req.body.time，req.body.addContent，req.body.userId，req.body.attention，req.body.category];
-    console.log(data);
+router.post('/add', function(req,res) {
+    var data=[req.body.title,req.body.content,req.body.urgent,req.body.score,req.body.anonymous,req.body.userId,req.body.category];
     questionDao.add(data,function(err,result){
     	o.result=err==null;
 		o.msg=o.result?"增加问题成功!":"增加问题失败!";
 		o.data={data:result};
 		res.json(o);	
     })
-    next();
 });
 //删除问题
-router.post('/del/:id', function(req, res,next) {
-	var data=[req.body.id];
-    console.log(data);
+router.post('/del/:id', function(req, res) {
+	var data=[req.params.id];
     questionDao.delete(data,function(err,result){
     	o.result=err==null;
 		o.msg=o.result?"删除问题成功!":"删除问题失败!";
 		o.data={data:result};
 		res.json(o);
     })
-    next();
 });
 //追加问题
-router.post('/add2', function(req,res,next) {
-    var data={
-    		req.body.id,
-    		req.body.addContent
-    }
-    console.log(data);
+router.post('/add2', function(req,res) {
+    var data=[req.body.id,req.body.addContent];
     questionDao.update(data,function(err,result){
     	o.result=err==null;
 		o.msg=o.result?"追加问题成功!":"追加问题失败!";
 		o.data={data:result};
 		res.json(o);
     })
-    next();
 });
 //查看问题
-router.get('/view/:id', function(req, res,next) {
+router.get('/view/:id', function(req, res) {
 	var data=[req.params.id];
-    console.log(data);
     questionDao.select*from(data,function(err,result){
     	o.result=err==null;
 		o.msg=o.result?"查询问题成功!":"查询问题失败!";
 		o.data={data:result};
 		res.json(o);	
     })
-    next();
 });
 module.exports=router;
